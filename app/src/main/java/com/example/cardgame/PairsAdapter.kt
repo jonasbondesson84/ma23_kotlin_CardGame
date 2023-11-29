521748a6105235c1bde14bf16b93fd8f140c41ad
Jonas Bondesson <jonasbondesson@yahoo.com>
2023-11-29 13:06:52 +0100

    fixed ui to show pairs

---
+++ app/src/main/java/com/example/cardgame/PairsAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.cardgame
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PairsAdapter(val context: Context, var numberOfPairs: Int): RecyclerView.Adapter<PairsAdapter.ViewHolder>() {
+
+    var layoutInflater = LayoutInflater.from(context)
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = layoutInflater.inflate(R.layout.pair_item, parent, false)
+
+        return ViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return numberOfPairs
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    fun updateNumberOfPairs(newNumberOfPairs: Int) {
+        numberOfPairs = newNumberOfPairs
+        notifyDataSetChanged()
+    }
+}
