159dc2899fea8d9e2d372e87e27ce7b1a9f770e4
Jonas Bondesson <jonasbondesson@yahoo.com>
2023-12-05 13:41:43 +0100

    recoded gameMode1

--- app/src/main/java/com/example/cardgame/GameEngine.kt
+++ app/src/main/java/com/example/cardgame/GameEngine.kt
@@ -7,9 +7,9 @@ object GameEngine {
         Level(2, "Rules for level 2:", 1000, 0, 0, R.drawable.beachball),
         Level(3, "Rules for level 3:", 1500, 0, 0, R.drawable.carrot),
         Level(4, "Rules for level 4:", 2000, 0, 0, R.drawable.tomato),
-        Level(5, "Rules for level 5: GAMEMODE GO FISH", 1, 0, 1, R.drawable.tree),
-        Level(6, "Rules for level 6:", 3, 0, 1, R.drawable.carrot),
-        Level(7, "Rules for level 7:", 5, 0, 1, R.drawable.bush),
+        Level(5, "Rules for level 5: GAMEMODE GO FISH", 100, 0, 1, R.drawable.tree),
+        Level(6, "Rules for level 6:", 300, 0, 1, R.drawable.carrot),
+        Level(7, "Rules for level 7:", 500, 0, 1, R.drawable.bush),
         Level(8, "Rules for level 8: GAMEMODE CRAZY 8", 100, 0, 2, R.drawable.beachball),
         Level(9, "Rules for level 9:", 10, 120, 2, R.drawable.tomato),
         Level(10, "Rules for level 10:", 10, 140, 2, R.drawable.fountain)
